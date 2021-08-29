e5b52721b58f93d204b184bb55cc65eb4b307ecf
Wahyu Nur Arizky <arizky.wna@gmail.com>
2021-08-29 07:41:16 +0700

    still get error

--- src/components/show/ShowGrid.js
+++ src/components/show/ShowGrid.js
@@ -7,19 +7,22 @@ import { useShows } from '../../misc/custom-hooks';
 const ShowGrid = ({ data }) => {
   const [starredShows, dispatchStarred] = useShows();
 
+  // memoize onStackClick only once for ShowGrid
+  // showId and isStarred are received from call in ShowCard.js
+  const onStarClick = useCallback(
+    (showId, isStarred) => {
+      if (isStarred) {
+        dispatchStarred({ type: 'REMOVE', showId });
+      } else {
+        dispatchStarred({ type: 'ADD', showId });
+      }
+    },
+    [dispatchStarred]
+  );
+
   return (
     <FlexGrid>
       {data.map(({ show }) => {
-        const isStarred = starredShows.includes(show.id);
-
-        // eslint-disable-next-line react-hooks/rules-of-hooks
-        const onStarClick = useCallback(() => {
-          if (isStarred) {
-            dispatchStarred({ type: 'REMOVE', showId: show.id });
-          } else {
-            dispatchStarred({ type: 'ADD', showId: show.id });
-          }
-        }, [isStarred, show.id]);
         return (
           <ShowCard
             key={show.id}
@@ -28,7 +31,7 @@ const ShowGrid = ({ data }) => {
             image={show.image ? show.image.medium : IMAGE_NOT_FOUND}
             summary={show.summary}
             onStarClick={onStarClick}
-            isStarred={isStarred}
+            isStarred={starredShows.includes(show.id)}
           />
         );
       })}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(
-  <HashRouter>
+  <BrowserRouter>
     <App />
-  </HashRouter>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
-serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register();
